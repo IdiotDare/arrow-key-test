de713402a3f9e8bbbdbe667553e0e5c2c4bd5d30
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2022-03-24 09:12:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,24 @@ document.onkeydown = function(e) {
             alert('up');
             break;
         case 39:
-            document.getElementById("first") = "  no script added";
             alert('right');
+            break;
+        case 40:
+            alert('down');
+            break;
+    }
+};
+//document.getElementById("first") = "  no script added";
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            alert('left');
+            break;
+        case 38:
+            alert('up');
+            break;
+        case 39:
+            document.getElementById("first") = "  no script added";
             break;
         case 40:
             alert('down');
